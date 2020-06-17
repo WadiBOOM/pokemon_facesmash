66f932577d9557b968ec50adbce2761beba1a577
Wadi BM <bmnwa@outlook.com>
2020-06-17 20:54:58 +0200

    adding controller routes and views

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,14 @@
 class PokemonsController < ApplicationController
+
+  def index
+    @pokemons = Pokemon.all
+  end
+
+  def update
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pokemons#index'
+
+  resources :pokemons, only: [:show]
 end
